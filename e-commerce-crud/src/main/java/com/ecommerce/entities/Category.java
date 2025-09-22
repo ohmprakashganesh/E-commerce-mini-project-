fa044c2331102faf13a945705963011b9ba835df
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2025-09-22 13:23:15 +0545

    added Dockerfile

--- e-commerce-crud/src/main/java/com/ecommerce/entities/Category.java
+++ e-commerce-crud/src/main/java/com/ecommerce/entities/Category.java
@@ -1,15 +1,12 @@
 package com.ecommerce.entities;
 
-
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
 @Entity(name = "Category")
 public class Category {
-
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -19,29 +16,6 @@ public class Category {
     @OneToMany(mappedBy = "category",cascade =CascadeType.ALL, orphanRemoval = true )
     private List<Product> products;
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public List<Product> getProducts() {
-//        return products;
-//    }
-//
-//    public void setProducts(List<Product> products) {
-//        this.products = products;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
+
 
 }
--- e-commerce-crud/src/main/java/com/ecommerce/entities/Product.java
+++ e-commerce-crud/src/main/java/com/ecommerce/entities/Product.java
@@ -1,6 +1,8 @@
 package com.ecommerce.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 
@@ -11,8 +13,11 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private  Long id;
+    @NotNull
     private String name;
+    @Min(0)
     private int price;
+    @Min(0)
     private  int stock;
 
     @ManyToOne
